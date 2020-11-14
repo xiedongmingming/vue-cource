4acd452921770d5cd9e5327c3e18411e6d821cc6
xieming <xieming@adwetec.com>
2020-11-14 16:51:26 +0800

    学习

--- vue.config.js
+++ vue.config.js
@@ -5,16 +5,26 @@ const resolve = dir => path.join(__dirname, dir)
 const BASE_URL = process.env.NODE_ENV === 'production' ? '/' : '/'
 
 module.exports = {
-  lintOnSave: false,
-  baseUrl: BASE_URL,
-  chainWebpack: config => {
-    config.resolve.alias
-      .set('@', resolve('src'))
-      .set('_c', resolve('src/components'))
-  },
-  // 打包时不生成.map文件
-  productionSourceMap: false,
-  devServer: {
-    proxy: 'http://localhost:3000'
-  }
+	lintOnSave: false,
+	baseUrl: BASE_URL,
+	chainWebpack: config => {
+		config.resolve.alias
+				.set('@', resolve('src'))
+				.set('_c', resolve('src/components'))
+		// config.module
+		// 		.rule('vue')
+		// 		.use('iview')
+		// 		.loader('iview-loader')
+		// 		.options({prefix: false})
+	},
+	// 打包时不生成.MAP文件
+	productionSourceMap: false,
+	devServer: {
+		proxy: 'http://localhost:3000'
+		//
+		// proxy: {
+		// 		'/api': 'http://localhost:3000'
+		// }
+		//
+	}
 }
