6f7bbcfe25f741a7c2138bb78046964265970e93
xieming <xieming@adwetec.com>
2020-11-14 17:06:53 +0800

    学习

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import '@/assets/font/iconfont.js'
 import '@/assets/font/iconfont.css'
 import IconFont from '_c/icon-font'
 import IconSvg from '_c/icon-svg'
+
 if (process.env.NODE_ENV !== 'production') require('./mock')
 
 Vue.config.productionTip = false
@@ -18,57 +19,57 @@ Vue.component('icon-svg', IconSvg)
 Vue.use(iview)
 
 const handleClick = event => {
-  console.log(event)
-  event.stopPropagation()
+	console.log(event)
+	event.stopPropagation()
 }
 
 let list = [{name: 'lison'}, {name: 'lili'}]
 const getLiEleArr = (h) => {
-  return list.map((item, index) => h('li', {
-    on: {
-      'click': handleClick
-    },
-    key: `list_item_${index}`
-  }, item.name))
+	return list.map((item, index) => h('li', {
+		on: {
+			'click': handleClick
+		},
+		key: `list_item_${index}`
+	}, item.name))
 }
 
 new Vue({
-  router,
-  store,
-  // render: h => {
-  //   return h(CountTo, {
-  //     'class': [],
-  //     attrs: {},
-  //     style: {},
-  //     props: {
-  //       endVal: 100
-  //     },
-  //     // domProps: {
-  //     //   innerHTML: '123'
-  //     // },
-  //     on: {
-  //       'on-animation-end': (val) => {
-  //         console.log('animation end!')
-  //       }
-  //     },
-  //     nativeOn: {
-  //       'click': () => {
-  //         console.log('click!')
-  //       }
-  //     },
-  //     directives: [],
-  //     scopedSlots: {},
-  //     slot: '',
-  //     key: '',
-  //     ref: ''
-  //   })
-  // }
-  // render: h => h('div', [
-  //   h('ul', {
-  //     on: {
-  //       'click': handleClick
-  //     }
-  //   }, getLiEleArr(h))
-  // ])
-  render: h => h(App)
+	router,
+	store,
+	// render: h => {
+	//   return h(CountTo, {
+	//     'class': [],
+	//     attrs: {},
+	//     style: {},
+	//     props: {
+	//       endVal: 100
+	//     },
+	//     // domProps: {
+	//     //   innerHTML: '123'
+	//     // },
+	//     on: {
+	//       'on-animation-end': (val) => {
+	//         console.log('animation end!')
+	//       }
+	//     },
+	//     nativeOn: {
+	//       'click': () => {
+	//         console.log('click!')
+	//       }
+	//     },
+	//     directives: [],
+	//     scopedSlots: {},
+	//     slot: '',
+	//     key: '',
+	//     ref: ''
+	//   })
+	// }
+	// render: h => h('div', [
+	//   h('ul', {
+	//     on: {
+	//       'click': handleClick
+	//     }
+	//   }, getLiEleArr(h))
+	// ])
+	render: h => h(App)
 }).$mount('#app')
